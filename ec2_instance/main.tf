b7d0c2198daca9ef251635b59a2b7d5e7111bd8a
djlittle825 <101017974+djlittle825@users.noreply.github.com>
2024-11-06 16:26:42 -0500

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = 'project1' 
+  key_name      = "project1" 
   tags = {
     Name = var.instance_name
   }
