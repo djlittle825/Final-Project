4a701bb45e12f5dd021bf0760ea127be8c130e10
djlittle825 <101017974+djlittle825@users.noreply.github.com>
2025-03-04 19:18:06 -0500

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = 'jenkins-key2024' 
+  key_name      = "jenkins-key2024" 
   tags = {
     Name = var.instance_name
   }
